d25129ece27e35af7c2a6b30a84b170735a0902c
wbstkr <wbstkr@gmail.com>
2022-11-23 22:54:18 -0500

    fixing indexing issue with drawSprite

--- src/wbstkr/util/SpriteSheet.java
+++ src/wbstkr/util/SpriteSheet.java
@@ -7,9 +7,9 @@ public class SpriteSheet {
     private final PApplet parent;
     private final PImage content;
     public final Sprite sprite;
-    public final int size;
+    private final int size;
 
-    private record Sprite(int width, int height) {
+    public record Sprite(int width, int height) {
     }
 
     public SpriteSheet(PApplet parent, String filename, int width, int height) {
@@ -19,18 +19,21 @@ public class SpriteSheet {
         this.size = (this.content.width / this.sprite.width) * (this.content.height / this.sprite.height);
     }
 
+    public int size() {
+        return this.size;
+    }
+
     // TODO: fix sprite not flipping with negative numbers
     // TODO: fix example file to correspond to this fix
 
     public void drawSprite(int index, float x, float y, float scaleX, float scaleY) {
         if (index < this.size) {
             int spriteX = (index % (this.content.width / this.sprite.width)) * this.sprite.width;
-            int spriteY = (index / (this.content.height / this.sprite.height)) * this.sprite.height;
+            int spriteY = (index / (this.content.width / this.sprite.width)) * this.sprite.height;
             parent.image(this.content, x, y, this.sprite.width * scaleX, this.sprite.height * scaleY, spriteX, spriteY,
                     spriteX + this.sprite.width, spriteY + this.sprite.height);
-        } else {
+        } else
             PApplet.println("Index is greater than size.");
-        }
     }
 
     public void drawSprite(int index, float x, float y, float scale) {
